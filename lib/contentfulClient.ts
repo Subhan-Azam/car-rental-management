98ffd943422c26f9684c16c674627423c05559bc
subhan <shiekhsubhan2005@gmail.com>
2025-04-08 18:15:01 -0700

    change in contentfullClient file

--- lib/contentfulClient.ts
+++ lib/contentfulClient.ts
@@ -1,4 +1,8 @@
-import {createClient} from "contentful";
+import { createClient } from "contentful";
+
+if (!process.env.SPACE_ID || !process.env.ACCESS_TOKEN) {
+  throw new Error("Missing SPACE_ID or ACCESS_TOKEN in environment variables.");
+}
 
 const client = createClient({
   space: process.env.SPACE_ID!,
