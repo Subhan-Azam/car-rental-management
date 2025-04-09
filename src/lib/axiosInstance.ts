b082e967acabd5c2d9ffaea5fc81dc012d58652e
subhan <shiekhsubhan2005@gmail.com>
2025-04-08 19:19:59 -0700

    changes in project

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://car-rental-management-1znd-mmf0gb6rn-subhanazams-projects.vercel.app/api",
   timeout: 5000,
 });
