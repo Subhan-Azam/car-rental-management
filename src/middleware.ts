42fa8eb214e989a32c77aa8db1e41f588b8a2e10
subhan <shiekhsubhan2005@gmail.com>
2025-04-10 19:45:12 -0700

    changes in project

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,45 @@
-// import { withAuth } from "next-auth/middleware";
-// import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-// export default withAuth(
-//   async function middleware(req) {
-//     const user = req.nextauth?.token;
+export default withAuth(
+  async function middleware(req) {
+    const user = req.nextauth?.token;
 
-//     if (!user || !user.role) {
-//       return NextResponse.json(
-//         { success: false, message: "Unauthorized" },
-//         { status: 401 }
-//       );
-//     }
+    if (!user || !user.role) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
 
-//     const pathname = req.nextUrl.pathname;
+    const pathname = req.nextUrl.pathname;
 
-//     if (user.role === "ADMIN") {
-//       return NextResponse.next();
-//     }
+    if (user.role === "ADMIN") {
+      return NextResponse.next();
+    }
 
-//     const allowedUserPaths = [
-//       "/dashboard/assets",
-//       "/dashboard/booking",
-//       "/dashboard/messages",
-//       "/dashboard/settings",
-//     ];
+    const allowedUserPaths = [
+      "/dashboard/assets",
+      "/dashboard/booking",
+      "/dashboard/messages",
+      "/dashboard/settings",
+    ];
 
-//     if (pathname.startsWith("/dashboard") && user.role === "USER") {
-//       if (!allowedUserPaths.includes(pathname)) {
-//         return NextResponse.redirect(new URL("/dashboard/booking", req.url));
-//       }
-//     }
+    if (pathname.startsWith("/dashboard") && user.role === "USER") {
+      if (!allowedUserPaths.includes(pathname)) {
+        return NextResponse.redirect(new URL("/dashboard/booking", req.url));
+      }
+    }
 
-//     return NextResponse.next();
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => !!token,
-//     },
-//   }
-// );
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
-// export const config = {
-//   matcher: ["/dashboard/:path*"],
-// };
-
-
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
