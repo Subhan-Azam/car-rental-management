e48198ea989077ad3d154fca28bc393a0f410c05
subhan <shiekhsubhan2005@gmail.com>
2025-04-18 17:03:17 -0700

    change baseurl

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://car-rental-management-khaki.vercel.app/api",
+  baseURL: "https://carrental-management.vercel.app/api",
+  // baseURL: "http://localhost:3000/api",
 });
